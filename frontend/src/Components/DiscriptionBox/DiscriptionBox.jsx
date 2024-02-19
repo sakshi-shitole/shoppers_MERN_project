ae8f918d807e92b45aab60f3c39bfa8b6ee3355d
sakshi-shitole <sakshishitole1@gmail.com>
2024-02-19 20:31:30 +0530

    Front-end part is successfully done.

---
+++ frontend/src/Components/DiscriptionBox/DiscriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DiscriptionBox.css'
+
+const DiscriptionBox = () => {
+    return (
+        <div className='discriptionbox'>
+            <div className='descriptionbox-navigator'>
+                <div className='descriptionbox-nav-box'>Description</div>
+                <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+            </div>
+            <div className='descriptionbox-description'>
+                <p>A website that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.13</p>
+            </div>
+        </div>
+    )
+}
+
+export default DiscriptionBox
---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import stardull_icon from '../Assets/star_dull_icon.png'
+import { ShopContext } from '../../Context/ShopContext'
+
+
+const ProductDisplay = (props) => {
+    const { product } = props;
+    const { addToCart } = useContext(ShopContext);
+    return (
+        <div className='productdisplay'>
+            <div className='productdisplay-left'>
+                <div className='productdisplay-img-list'>
+                    <img src={product.image} alt='' />
+                    <img src={product.image} alt='' />
+                    <img src={product.image} alt='' />
+                    <img src={product.image} alt='' />
+                </div>
+                <div className='productdisplay-img'>
+                    <img className='productdisplay-main-img' src={product.image} alt='' />
+                </div>
+            </div>
+            <div className='productdisplay-right'>
+                <h1>{product.name}</h1>
+                <div className='productdisplay-right-stars'>
+                    <img src={star_icon} alt='' />
+                    <img src={star_icon} alt='' />
+                    <img src={star_icon} alt='' />
+                    <img src={star_icon} alt='' />
+                    <img src={stardull_icon} alt='' />
+                    <p>(122)</p>
+                </div>
+                <div className='productdisplay-right-prices'>
+                    <div className='productdisplay-right-prices-old'>
+                        ${product.old_price}
+                    </div>
+                    <div className='productdisplay-right-prices-new'>
+                        ${product.new_price}
+                    </div>
+                </div>
+                <div className='productdisplay-right-description'>
+                    A loose, long-sleeved, collarless pullover of soft, absorbent fabric, as cotton jersey, with close-fitting or elastic cuffs and sometimes a drawstring at the waist, commonly worn during athletic activity for warmth or to induce sweating.
+                </div>
+                <div className='productdisplay-right-size'>
+                    <h1>Select Size</h1>
+                    <div className='productdisplay-right-sizes'>
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                </div>
+                <button onClick={() => { addToCart(product.id) }}>ADD TO CART</button>
+                <p className='productdisplay-right-category'><span>Category :</span> Women, t-shirt, Crop Top </p>
+                <p className='productdisplay-right-category'><span>Tags :</span> Modern, Latest </p>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDisplay
